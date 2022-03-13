db01faf594903c53b7c4f08cde5104288f902035
Sabiha khair ohi <ohiislam3@gmail.com>
2022-03-13 15:09:27 +0600

    Test

---
+++ src/Componants/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import { Box, Grid, Text } from '@chakra-ui/react'
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <Box>
+             <Grid>
+                 
+             </Grid>
+        </Box>
+    )
+}
+
+export default Navbar
